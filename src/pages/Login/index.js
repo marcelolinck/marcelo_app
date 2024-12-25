2d49609c76fb1252940f138fb0a892417748bf7e
marcelolinck <malveslinck@gmail.com>
2024-12-24 23:35:07 -0400

    Implementado o cadastrar no aplicativo.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -9,7 +9,11 @@ import {
   TouchableOpacity,
   Alert,
 } from "react-native";
+
+//Arquivo com as configurações da API
 import api from "../../config/api";
+
+import * as yup from "yup";
 
 export default function Login() {
   // Navegar entre as telas
@@ -20,51 +24,77 @@ export default function Login() {
   const [password, setPassword] = useState("");
 
   const loginSubmit = async () => {
-    
-  //Funcao para validar o formulario
-    if (!validate()) return;
-  
+    //Funcao para validar o formulario com js puro
+    //if (!validate()) return;
 
-    await api
-      .post("/login", { email, password })
-      .then((response) => {
-        console.log(response)
+    try {
+      //Funcao para validar o formulario com yup
+      await validationSchema.validate(
+        { email, password },
+        { abortEarly: false }
+      );
 
-        Alert.alert("Sucesso", response.data.user.email)
-      })
-      .catch((err) => {
-         console.log(err.response);
+      //Requisicao para a API indicand a rota e os dados
+      await api
+        .post("/login", { email, password })
+        .then((response) => {
+          console.log(response);
 
-        if (err.response) {
-            Alert.alert("Ops!", err.response.data.message)
-        }else
-        {
-          Alert.alert("Ops!", "Erro, tente novamente!")
-        }
-      });
+          Alert.alert("Sucesso", response.data.user.email);
+        })
+        .catch((err) => {
+          //Acessar o cacth quando a api retornar status sucesso.
+          console.log(err.response);
+
+          if (err.response) {
+            //Acessa o IF quando a API retornar erro
+            Alert.alert("Ops!", err.response.data.message);
+          } else {
+            //Acessa o ELSE quando a API não responder.
+            Alert.alert("Ops!", "Erro, tente novamente!");
+          }
+        });
+    } catch (error) {
+      //Acessa o catch se o houver erro no try
+      Alert.alert("Ops!", error.errors[0]);
+    }
   };
 
   //Funcao que valida o formulário
-  const validate  = () =>{
-    if(!email){
-      Alert.alert("Ops", "Erro: necessário preencher o campo do e-mail")
-      return false;
-    }
-    if(!password){
-      Alert.alert("Ops", "Erro: necessário preencher o campo de senha")
-      return false;
-    }
-    return true
-  }
+  // const validate  = () =>{
+  //   if(!email){
+  //     Alert.alert("Ops", "Erro: necessário preencher o campo do e-mail")
+  //     return false;
+  //   }
+  //   if(!password){
+  //     Alert.alert("Ops", "Erro: necessário preencher o campo de senha")
+  //     return false;
+  //   }
+  //   return true
+  // }
+
+  //Funcao que valida o formulário com yup
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string("Necessário preencher o campo usuário!")
+      .required("Necessário preencher o campo usuário!")
+      .email("Necessário informar um e-mail válido"),
+    password: yup
+      .string("Necessário preencher o campo da senha!")
+      .required("Necessário preencher o campo da senha!"),
+  });
 
   return (
     <View style={styles.container}>
+      {/* View para carregamento do logo */}
       <View style={styles.logo}>
         <Image
           source={require("../../../assets/logo1.png")}
           resizeMode="stretch"
         />
       </View>
+
+      {/* Campo de usuáro */}
       <TextInput
         placeholder="Usuário"
         style={styles.inputform}
@@ -74,6 +104,7 @@ export default function Login() {
         value={email}
         onChangeText={(text) => setEmail(text)}
       />
+      {/* Campo de senha */}
       <TextInput
         style={styles.inputform}
         placeholder="Senha"
@@ -84,16 +115,19 @@ export default function Login() {
         onChangeText={(text) => setPassword(text)}
       />
 
+      {/* Criar o botão para submeter/enviar os dados do formulário */}
       <TouchableOpacity style={styles.btnSubmitForm} onPress={loginSubmit}>
         <Text style={styles.txtSubmitForm}>Acessar</Text>
       </TouchableOpacity>
-
+      
+      {/* Link para novo usuario */}
       <Text
         style={styles.linkLogin}
         onPress={() => Navigation.navigate("NewUser")}
       >
         Cadastrar
       </Text>
+      {/* Link para recuperar a senha */}
       <Text
         style={styles.linkLogin}
         onPress={() => Navigation.navigate("RecoverPassword")}
@@ -103,7 +137,7 @@ export default function Login() {
     </View>
   );
 }
-
+// Personalizar a tela de login com css
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- src/pages/NewUser/index.js
+++ src/pages/NewUser/index.js
@@ -1,13 +1,174 @@
-import { View, Text } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+//useState - Adicionar o estado ao componente
+import { useState } from "react";
+
+import * as yup from "yup";
+//Arquivo com as configurações da API
+import api from "../../config/api";
 
 export default function NewUser() {
-  const Navigation = useNavigation()
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Screen NewUser</Text>
+  const Navigation = useNavigation();
 
-      <Text onPress={() =>{Navigation.navigate('Login')}}>Login</Text>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const addUser = async () => {
+    //Usar o try e catch para gerenciar exceção/erro
+    try {
+      //Funcao para validar o formulario com yup
+      await validationSchema.validate(
+        { name, email, password },
+        { abortEarly: false }
+      );
+      //Requisicao para a aAPI indicando a rota e os dados
+      await api
+        .post("register", { name, email, password })
+        .then((response) => {
+          Alert.alert("Sucesso", response.data.message);
+
+          Navigation.navigate("Login");
+        })
+        .catch((err) => {//Acessar o catch quando a api retornar status de erro.
+          //Recebe os erros da api e atribui na constante errors
+          const errors = err.response?.data?.erros;
+
+          if (err.response) {
+            const message = `${errors?.name ?? ""} ${errors?.name ? "\n" : ""}${errors?.email ?? ""}${errors?.email ? "\n" : ""}
+            ${errors?.password ?? ""} ${errors?.password ? "\n" : ""}`;
+
+            //Acessa o IF quando a API retornar erro
+            Alert.alert("Ops!", message.trim());
+          } else {
+            //Acessa o ELSE quando a API não responder.
+            Alert.alert("Ops!", "Erro, tente novamente!");
+          }
+        });
+
+      //Permanece no try se não houver nenhum erro
+    } catch (error) {
+      //Acessa o catch quando houver um erro no try
+      if (error.errors) {
+        //Acessa o if quanto existir a mensagem de erro
+        Alert.alert("Ops", error.errors[0]);
+      } else {
+        Alert.alert("Ops", "Erro: tente novamente!");
+      }
+    }
+  };
+
+  //Funcao que valida o formulário com yup
+  const validationSchema = yup.object().shape({
+    name: yup
+      .string("Necessário preencher o campo nome!")
+      .required("Necessário preencher o campo nome!"),
+    email: yup
+      .string("Necessário preencher o campo e-mail!")
+      .required("Necessário preencher o campo e-mail!")
+      .email("Necessário informar um e-mail válido"),
+    password: yup
+      .string("Necessário preencher o campo da senha!")
+      .required("Necessário preencher o campo da senha!")
+      .min(8, "A senha deve ter no mínimo 8 caracteres!"),
+  });
+
+  return (
+    <View style={styles.container}>
+      {/* View para carregamento do logo */}
+      <View style={styles.logo}>
+        <Image
+          source={require("../../../assets/logo1.png")}
+          resizeMode="stretch"
+        />
+      </View>
+      {/* Campo de usuáro */}
+      <TextInput
+        placeholder="Nome completo"
+        style={styles.inputform}
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+      {/* Campo de usuáro */}
+      <TextInput
+        placeholder="Melhor e-mail"
+        style={styles.inputform}
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="email-address"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      {/* Campo de senha */}
+      <TextInput
+        style={styles.inputform}
+        placeholder="Senha com no mínimo 6 caracteres"
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      {/* Criar o botão para submeter/enviar os dados do formulário */}
+      <TouchableOpacity style={styles.btnSubmitForm} onPress={() => addUser()}>
+        <Text style={styles.txtSubmitForm}>Acessar</Text>
+      </TouchableOpacity>
+
+      {/* Link para a tela de login */}
+      <Text
+        style={styles.linkLogin}
+        onPress={() => Navigation.navigate("Login")}
+      >
+        Login
+      </Text>
     </View>
   );
 }
+
+// Personalizar a tela de login com css
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#10101c",
+  },
+  logo: {
+    paddingBottom: 20,
+  },
+  inputform: {
+    backgroundColor: "#f5f5f5",
+    width: "90%",
+    marginBottom: 15,
+    color: "#10101c",
+    fontSize: 18,
+    borderRadius: 6,
+    padding: 10,
+  },
+  btnSubmitForm: {
+    backgroundColor: "#1f51fe",
+    width: "90%",
+    height: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 6,
+  },
+  txtSubmitForm: {
+    color: "#f5f5f5",
+    fontSize: 18,
+  },
+  linkLogin: {
+    color: "#1f51fe",
+    marginTop: 10,
+    fontSize: 18,
+  },
+});
