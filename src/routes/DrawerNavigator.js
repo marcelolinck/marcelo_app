81006caf5d1553e95d69c4ece5053faeefc81785
marcelolinck <malveslinck@gmail.com>
2025-02-09 17:46:19 -0400

    Implementado o botao sair dentro do drawer navigation.

--- src/routes/DrawerNavigator.js
+++ src/routes/DrawerNavigator.js
@@ -1,31 +1,61 @@
+//useContext - compartilhar dados entre as páginas
+import { useContext } from "react";
+
 import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList,
+} from "@react-navigation/drawer";
 import Home from "../pages/Home";
 import Billys from "../pages/Billys";
 import Revenues from "../pages/Revenues";
 
+//Importar o contexto de autenticação para verificar se o usuario está logado
+import { AuthContext } from "../contexts/authContext";
+
 const Drawer = createDrawerNavigator();
+
+
+//Criar a função para sair do app
+function DrawerSignOut(props) {
+  const { signOut } = useContext(AuthContext);
+
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem label="Sair" onPress={() => signOut()} />
+    </DrawerContentScrollView>
+  );
+}
 
 export default function DrawerNavigator() {
   return (
-    <Drawer.Navigator initialRouteName="Home">
+
+    //Nesse caso, o DrawerNavigator vai receber o DrawerSignOut para que o usuário possa sair do app
+    <Drawer.Navigator drawerContent={props => <DrawerSignOut {...props} />}>
       <Drawer.Screen
         name="Home"
         component={Home}
         options={{
           drawerLabel: "Home",
-          
         }}
       />
-      <Drawer.Screen name="Billys" component={Billys} 
-      options={{
-        drawerLabel: "Contas",
-      }}
+      <Drawer.Screen
+        name="Billys"
+        component={Billys}
+        options={{
+          drawerLabel: "Contas",
+        }}
       />
-      <Drawer.Screen name="Revenues" component={Revenues} 
-      options={{
-        drawerLabel: "Receitas",
-      }}/>
+      <Drawer.Screen
+        name="Revenues"
+        component={Revenues}
+        options={{
+          drawerLabel: "Receitas",
+        }}
+      />
     </Drawer.Navigator>
   );
 }
